f6bef73112eb7d7a1714de2f7ce2c000ff19d381
i314q159 <i314q159@outlook.com>
2025-07-30 07:58:17 +0800

    Remove .vscode from repository

--- README.md
+++
@@ -1,7 +0,0 @@
-# MY NEOVIM CONFIG
-
-## For Windows
-
-```ps1
-cp init.lua ~/AppData/Local/nvim/init.lua
-```
--- sync.ps1
+++
@@ -1,4 +0,0 @@
-cp ~/AppData/Local/nvim/init.lua init.lua
-git add .
-git commit -m "update init.lua"
-git push
